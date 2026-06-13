f340a4deb9379c3f76f4a339c406b27e56cfaaf5
yesblue0342-bit <yesblue0342@gmail.com>
2026-06-13 16:31:41 +0900

    Update drive-utils.js

--- api/drive-utils.js
+++ api/drive-utils.js
@@ -1,1 +1,14 @@
-*
+export async function saveToDrive(data) {
+  return {
+    success: true,
+    message: "Google Drive Save Ready",
+    data
+  };
+}
+
+export async function loadFromDrive() {
+  return {
+    success: true,
+    files: []
+  };
+}
